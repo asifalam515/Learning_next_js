e23dd2ef6bc755555ec69cbb729a341ab9039f9e
asifalam515 <asibulalam515@gmail.com>
2026-02-05 08:05:07 +0600

    blog page

--- blog_app_nextjs/app/(commonLayout)/page.tsx
+++ blog_app_nextjs/app/(commonLayout)/page.tsx
@@ -1,13 +1,22 @@
-import { Button } from "@/components/ui/button";
+import BlogCard from "@/components/modules/homepage/BlogCard";
+import { BlogPost } from "@/types/blog.type";
 import { blogService } from "../services/blog.service";
 
 export default async function HomePage() {
-  const { data } = await blogService.getBlogPost();
+  const { data } = await blogService.getBlogPost(
+    {
+      isFeatured: false,
+    },
+    {
+      cache: "no-store",
+    },
+  );
   console.log(data);
   return (
-    <div>
-      <h1 className="text-5xl">This is Home</h1>
-      <Button variant="outline">Click Here</Button>
+    <div className="grid grid-cols-3 max-w-7xl mx-auto px-4 gap-2">
+      {data.data?.map((post: BlogPost) => (
+        <BlogCard key={post.id} post={post}></BlogCard>
+      ))}
     </div>
   );
 }

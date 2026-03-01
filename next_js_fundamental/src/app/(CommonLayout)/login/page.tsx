6f48c9a140a4c1e0b7784a86c2a72f66856f8de7
asifalam515 <asibulalam515@gmail.com>
2026-03-01 19:33:15 +0600

    added

--- next_js_fundamental/src/app/(CommonLayout)/login/page.tsx
+++ next_js_fundamental/src/app/(CommonLayout)/login/page.tsx
@@ -1,5 +1,11 @@
+import LoginForm from "@/components/modules/Auth/LoginForm";
+
 const LoginPage = () => {
-  return <div></div>;
+  return (
+    <div className="h-[80vh] flex justify-center items-center">
+      <LoginForm></LoginForm>
+    </div>
+  );
 };
 
 export default LoginPage;
--- next_js_fundamental/src/services/auth/index.ts
+++ next_js_fundamental/src/services/auth/index.ts
@@ -1,16 +1,26 @@
-export const LoginUser = async (userData: any) => {
+"use server";
+import { cookies } from "next/headers";
+import { FieldValues } from "react-hook-form";
+
+export const LoginUser = async (userData: FieldValues) => {
   try {
     const res = await fetch("http://localhost:5000/api/auth/sign-in/email", {
       method: "POST",
+
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(userData),
     });
     const result = await res.json();
+
+    console.log(result);
+    const storedCookies = await cookies();
+    if (result.success) {
+      storedCookies.set("token", result.data.accessToken);
+    }
     return result;
   } catch (error: any) {
-    console.log(error);
     return Error(error);
   }
 };
